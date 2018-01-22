d2b0bfc0cdab6f55de8f18ee7c3516c1334edbcf
xgu60 <xgu60@gatech.edu>
2018-01-22 00:40:54 -0500

    plot

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-    EPOCH_NUM = 10
-    BATCH_SIZE = 64
+    EPOCH_NUM = 100
+    BATCH_SIZE = 32
 
     data = MerckChallengeDataset("ACT7_competition_training.csv")
     train_data = DataLoader(dataset=data, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
@@ -17,16 +18,41 @@ if __name__ == "__main__":
     criterion = nn.MSELoss()
     optimizer = optim.Adam(net.parameters(), lr=0.05, betas=(0.9, 0.99))
 
+    #plot
+    plt.ion()    
+    start = 0
+    xdata = []
+    ydata = []
+
+
     for epoch in range(EPOCH_NUM):
 	    for i, (inputs, labels) in enumerate(train_data):		
 		    inputs, labels = Variable(inputs), Variable(labels)
 		    y_pred = net(inputs)
 		    loss = criterion(y_pred, labels)
-		    print(EPOCH_NUM, i, loss)
+		    #print(EPOCH_NUM, i, loss)
+
+		    #update plot
+		    xdata.append(start)
+		    start += 1
+		    ydata.append(loss.data[0])
+
+		    plt.plot(xdata, ydata, 'r-')
+		    plt.yscale('log')
+		    plt.pause(0.5)
+		    plt.show()
+		    
+
+		    
+
 
 		    optimizer.zero_grad()
 		    loss.backward()
 		    optimizer.step()
+
+    
+
+    
 
 
 
